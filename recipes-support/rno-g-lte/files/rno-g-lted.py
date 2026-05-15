dbf3b373485424014fd22fd29c10730f63ce9d35
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2026-05-15 16:38:10 -0500

    add lte recipe

---
+++ recipes-support/rno-g-lte/files/rno-g-lted.py
@@ -0,0 +1,304 @@
+#! /usr/bin/env python3
+
+# This continually checks for the LTE modem and 
+
+#TODO rewrite in C or maybe bash if we need the free memory 
+#    Note: this uses ~7 MB of RAM in python, plus ~2 MB transiently to run ping.  
+
+
+# Cosmin Deaconu <cozzyd@kicp.uchicago.edu> 
+
+import serial 
+import sys 
+import os 
+import re 
+import time
+import signal 
+import subprocess 
+import sdnotify 
+
+#this doesn't seem to help... 
+import gc 
+
+
+ENABLE_MONI=not os.path.exists("/rno-g/cfg/lte-disable-moni"); 
+ENABLE_MCU_RESET=not os.path.exists("/rno-g/cfg/lte-disable-mcu-reset") 
+ENABLE_USB_RESET=not os.path.exists("/rno-g/cfg/lte-disable-usb-reset") 
+
+acm = None 
+host = "10.1.0.1"
+ping_count = 5 
+check_serial_sleep_amt = 60 
+check_connection_sleep_amt = 30 
+interrupt_flag = False 
+moni_file = None 
+
+
+
+notifier = sdnotify.SystemdNotifier() 
+
+# read a line and convert it to a string
+def rl(): 
+    r = acm.readline().decode("utf-8") 
+#    print(r) 
+    return r
+
+def moni():  
+    acm.write("AT#MONI\r\n".encode("utf-8")); 
+    res = "None\r\n"
+    line = None
+    while line != "OK\r\n": 
+        line = rl(); 
+        if line.startswith("#MONI"):
+            res = line
+    moni_file.write(str(time.time())+":"+res); 
+    moni_file.flush() 
+
+def interruptible_sleep(t, max_sleep =1): 
+    global interrupt_flag 
+    desired_end = time.time()+t; 
+    while not interrupt_flag: 
+        left = desired_end-time.time() 
+        if left <= 0:
+            break 
+        sleep_amt = left if left < max_sleep else max_sleep 
+        time.sleep(sleep_amt) 
+        
+    #reset the interrupt
+    interrupt_flag = False 
+
+# write a line, eat the echoed and blank line 
+def wl(text):
+    acm.write(text.encode("utf-8"))
+    rl() #echoed line
+    rl() #blank line 
+
+def check_ok(cmd, throw_exception = True):
+    wl(cmd)
+    r = rl(); 
+    if  r!= 'OK\r\n':
+        if throw_exception: 
+            raise IOError("did not get an ok from " + cmd + " , got: "+r)
+        print ("did not get an ok from " + cmd +"+ , got: "+r)
+        return 1
+    return 0 
+
+def receive_signal(signum, stack): 
+    global interrupt_flag
+    print('received ',signum) 
+    interrupt_flag = True
+
+
+def check_serial(): 
+    return os.path.exists("/dev/ttyLTE")
+
+def run(cmd): 
+    print(cmd) 
+    os.system(cmd) 
+
+
+def wwan0_up(): 
+    with open("/sys/class/net/wwan0/operstate") as f: 
+        state = f.readline(); 
+        print("wwan0 is " + state); 
+        return state == "up\n" 
+
+
+def check_connection(): 
+
+    if not wwan0_up(): 
+        return 1
+
+    #ping 
+    return subprocess.call(["ping","-c", str(ping_count),"-n","-q", host])
+
+
+
+def open_serial(): 
+    global acm 
+    acm = serial.Serial("/dev/ttyLTE",115200,timeout=1, write_timeout=1)   
+
+def try_to_connect(): 
+
+
+    #flush a line 
+    wl('\r\n') 
+    acm.flush() 
+
+    # Check for ok 
+    if (check_ok("AT\r\n")): 
+        return 1 
+
+    wl('AT#RFSTS\r\n'); 
+    rfsts = rl() 
+    print(rfsts); 
+    rl() #blank line
+    rl() #ok line
+
+    # Get the ip address 
+    wl('AT+CGPADDR=1\r\n') 
+    addrline = rl() 
+    rl() # blank line 
+    rl() # ok line 
+    
+    print(addrline) 
+    match = re.search('\+CGPADDR: 1,\"(\S+)\"',addrline)
+    print(match) 
+    if match == None or len(match.groups()) < 1: 
+        print ("Didn't get an IP?")
+        return 2
+
+    addr = match.groups()[0] 
+    print("IP address is %s" %(addr))
+
+    #Now let's set up the modem properly 
+    if (check_ok('AT#NCM=1,1\r\n')): return 1
+    wl('AT+CGDATA="M-RAW_IP",1\r\n') 
+
+    #Wait for either a connect or error 
+    while True: 
+        text = rl() 
+        if text =='CONNECT\r\n':
+            print("Connected!\n") 
+            break
+        if text =='ERROR\r\n': 
+            print("Didn't connect :(")
+            return 3 
+
+    if wwan0_up(): 
+        run("ifconfig wwan0 down")
+
+    run("ip addr flush dev wwan0") 
+
+    #set up the connection 
+    run("ifconfig wwan0 %s netmask 255.0.0.0 up" % (addr))
+    time.sleep(1) 
+
+    #set up the routes
+    run("ip route add 10.2.0.0/24 via 10.2.0.1"); 
+
+    run("ip route add default via 10.2.0.1"); 
+    run('if  systemctl --quiet is-enabled systemd-resolved ; then echo "using systemd-resolved"; else echo "nameserver 8.8.8.8" > /etc/resolv.conf ; fi ' )
+    time.sleep(10) 
+    return  0
+
+def reboot_modem_via_uc(): 
+    if ENABLE_MCU_RESET: 
+        print ("Trying to restart modem via micro") 
+        time.sleep(1)
+        os.system('echo "#LTE-OFF" > /dev/ttyController') 
+        time.sleep(15)
+        os.system('echo "#LTE-ON" > /dev/ttyController') 
+        time.sleep(15)
+    else: 
+        print("Would have restarted modem via micro but not allowed"); 
+        time.sleep(30) 
+
+def reboot_modem(): 
+    if ENABLE_USB_RESET: 
+        print ("Trying to restart modem directly") 
+        return check_ok("AT#ENHRST=1,0\r\n",False)  #reboot router 
+    else: 
+        print("Would have restart modem via USB but not allowed") 
+        return 1 
+
+
+if __name__=="__main__": 
+
+    signal.signal(signal.SIGUSR1,receive_signal) 
+
+    time.sleep(5) #wait five seconds to avoid dying too quickly
+    print("Started!"); 
+
+    print("Monitoring", "enabled" if ENABLE_MONI else "disabled")
+    print("MCU Reset", "enabled" if ENABLE_MCU_RESET else "disabled")
+    print("USB Reset", "enabled" if ENABLE_USB_RESET else "disabled")
+
+    if ENABLE_MONI: 
+        #TODO use log rotator here, with compression 
+        try: 
+            moni_file = open("/data/lte.log","a") 
+        except IOError as e: 
+            print(str(e))
+            print ("Using /rno-g/var/lte.log instead") 
+            moni_file = open("/rno-g/var/lte.log","a") 
+
+
+    while True: 
+        print("Kicking watchdog"); 
+        sys.stdout.flush()
+        notifier.notify("WATCHDOG=1") 
+
+        if not check_serial(): 
+            if acm is not None: 
+                acm.close() 
+                acm = None 
+
+            # give it a bit of time... 
+            interruptible_sleep(check_serial_sleep_amt) 
+            if not check_serial():
+                reboot_modem_via_uc() 
+                interruptible_sleep(check_serial_sleep_amt) 
+        else:  
+            ntries = 0; 
+            while acm is None:
+               try: 
+                   print("Opening serial") 
+                   open_serial()
+                   time.sleep(3) 
+                   check_ok("AT\r\n") 
+                   time.sleep(1) 
+               except IOError: 
+                   print("error while reading serial (try %d) " % (ntries)) 
+                   ntries+=1 
+                   if (ntries > 3):
+                       reboot_modem_via_uc(); 
+                       time.sleep(15) 
+                       break 
+                   acm = None 
+               time.sleep(5) 
+
+            if acm is None: 
+                continue 
+
+            if ENABLE_MONI: 
+                moni() 
+            if not check_connection(): 
+                interruptible_sleep(check_connection_sleep_amt) 
+            else: 
+
+                print("Connection check failed") 
+                time.sleep(5) 
+                success= False; 
+                for i in range(5): 
+                    if not check_connection(): 
+                        print("Connection check is actually ok") 
+                        success = True
+                    else: 
+                        success = not try_to_connect() 
+                    if success: 
+                        time.sleep(5) 
+                        break 
+                    interruptible_sleep(30); 
+                if not success: 
+                    check_ok("AT+COPS=0\r\n") #make sure automatic network selection 
+                    time.sleep(5) 
+                    reboot_modem() 
+                    acm.close() 
+                    acm = None
+                    time.sleep(30) 
+
+
+        gc.collect() 
+
+
+
+
+    sys.stdout.flush()
+
+
+
+
+
+
+
---
+++ recipes-support/rno-g-lte/rno-g-lte.bb
@@ -0,0 +1,33 @@
+SUMMARY = "LTE stuff for RNO-G Rev N"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+
+SRC_URI = "file://10-wwan0.link file://90-lte.rules file://is-lte-up file://rno-g-lte-trigger file://rno-g-lted.service file://rno-g-lted.py file://setup-lte "
+
+inherit systemd
+
+SYSTEMD_SERVICE:${PN} = "rno-g-lted.service"
+SYSTEMD_AUTO_ENABLE:${PN} = "enable"
+
+do_install() {
+      # Create the target directory for udev
+         install -d ${D}${sysconfdir}/udev/rules.d
+         install -m 0644 ${WORKDIR}/90-lte.rules ${D}${sysconfdir}/udev/rules.d/
+
+         install -d ${D}/rno-g/bin
+         install -m 0755 ${WORKDIR}/rno-g-lte-trigger ${D}/rno-g/bin
+         install -m 0755 ${WORKDIR}/is-lte-up ${D}/rno-g/bin
+         install -m 0755 ${WORKDIR}/setup-lte ${D}/rno-g/bin
+         install -m 0755 ${WORKDIR}/rno-g-lted.py ${D}/rno-g/bin/rno-g-lted
+
+         install -d ${D}${systemd_system_unitdir}
+         install -m 0644 ${WORKDIR}/rno-g-lted.service ${D}${systemd_system_unitdir}
+
+         install -d ${D}/${sysconfdir}/systemd/network
+         install -m 0644 ${WORKDIR}/10-wwan0.link ${D}/${sysconfdir}/systemd/network
+}
+
+FILES:${PN} += "${sysconfdir}/udev/rules.d/90-lte.rules /rno-g/bin/is-lte-up /rno-g/bin/setup-lte /rno-g/bin/rno-g-lte-trigger /rno-g/bin/rno-g-lted ${systemd_system_unitdir}/rno-g-lted.service ${sysconfdir}/network/10-wwan0.link"
+
+RDEPENDS:${PN} +=" python3-sdnotify "
